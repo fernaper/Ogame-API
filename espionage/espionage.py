5e877d6e7612a1832a9d79c3899559693215e55b
Fernando Pérez <fernaperg@gmail.com>
2018-06-08 18:02:47 +0200

    v0.0.2
    From now on, he is able to read a complete espionage report in Spanish

--- espionage/espionage.py
+++ espionage/espionage.py
@@ -1,10 +1,6 @@
-def read_espionage(espionage, lang='es'):
-    if lang == 'es':
-        ans = read_espionage_spanish(espionage)
-    return ans
-
-def read_espionage_spanish(espionage):
+def read_espionage(espionage, structures):
     ans = {}
+    carefull = False
     
     espionage = espionage.split('\n')
     line = espionage[0].split(' ')
@@ -12,22 +8,64 @@ def read_espionage_spanish(espionage):
     ans.update({'coordinates':line[5]})
     ans.update({'date':'{} {}'.format(line[6], line[7])})
     
-    i = 1
-    # I want to find the line with his name
-    while espionage[i][0] != 'J':
-        i+=1
-    ans.update({'name':' '.join(espionage[i].split(' ')[1:])[1:]})
+    i = 2
+    name = ' '.join(espionage[i].split(' ')[1:])[1:]
+    pos = name.find('(i')
     
-    i+=1
+    if pos != -1:
+        name = name[:pos]
+    
+    ans.update({'name':name})
+    ans.update({'inactive':pos!=-1})
+    
+    i += 1
     ans.update({'counterspionage':espionage[i].split(' ')[3]})
     
-    i+=3
+    i += 3
     line = espionage[i].split(' ')
-    ans.update({'metal':line[0]})
-    ans.update({'crystal':line[1]})
-    ans.update({'deuteryum':line[2]})
-    ans.update({'energy':line[3]})
+    resources = {}
+    resources.update({'metal':line[0]})
+    resources.update({'crystal':line[1]})
+    resources.update({'deuteryum':line[2]})
+    resources.update({'energy':line[3]})
+    ans.update({'resources':resources})
     
-    i+=2
+    pipeline = ['ships','defense','buildings','research']
+    
+    for element in pipeline:
+        i += 2
+        line = espionage[i].replace('.','')
+        care, entities = read_entities(line,structures[element])
+        if care:
+            carefull = True
+        ans.update({element:entities})
+    
+    ans.update({'care':carefull})
     
     return ans
+
+def read_entity(structure,line):
+    num_structure = 0
+    #print(':' + structure + ': ---> :' + line[:len(structure)] + ':')
+    if structure == line[:len(structure)]:
+        line = line[len(structure)+1:]
+        j = 1
+        while j-1 < len(line) and line[:j].isdigit():
+            j += 1
+        num_structure = int(line[:j-1])
+        line = line[j-1:]
+        
+    return line, num_structure
+
+def read_entities(line, structure):
+    ans = {}
+    length = len(line)
+    while line != '':
+        for s in structure:
+            line, num = read_entity(structure[s], line)
+            if num != 0:
+                ans.update({s:num})
+        if len(line) == length:
+            break;
+    
+    return len(ans) == 0, ans
